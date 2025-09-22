448511369a8c0970d83a0cdbbd2546362b4df278
MathContest <jjz@seu.edu.cn>
2025-09-22 21:35:43 +0800

    zjj

--- Test.cpp
+++ Test.cpp
@@ -1,7 +1,30 @@
 // 写一个hello world程序
 #include <iostream> 
+#include <vector>
+using namespace std;
+
+
+struct OperatorNode{
+    int id;
+    string op;
+    string pipe;
+    int cycles;
+    vector<string> bufIds;
+};
+
+struct CacheNode{
+    int id;
+    string op;
+    string bufId;
+    int size;
+    string type;
+};
+
+
+
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;  
-    
+
 }
